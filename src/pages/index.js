491065616dc17877431d439d0725efdb6c80f4c5
walanpat <39136284+walanpat@users.noreply.github.com>
2021-02-08 15:20:22 -0500

    Initial Push
    Initial work started

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,150 +1,112 @@
-import * as React from "react"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: "300",
-  fontSize: "24px",
-  maxWidth: "560px",
-}
-
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: "16px",
-  verticalAlign: "5%",
-}
-
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
-
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: "14px",
-}
-
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-// data
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#000000",
-  },
-]
+import * as React from 'react';
+import '../styles.css';
 
 // markup
+
 const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
+    <main>
+      <title>Paw Pals</title>
+      <div
+        className='flexWrapper'
+        style={{ justifyContent: 'center', flexDirection: 'column' }}
+      >
+        <h1>Paw Pals</h1>
+        <h2>A Dating app but for dogs!</h2>
+      </div>
+      <h3>Header 2</h3>
+      <p>
+        PawPals is a social media site, but for dog owners. Similar to other
+        dating apps, PawPals users create a profile for their pup, and “swipe”
+        left or right on other dogs’ profiles to find playmates for their own.
+        When two PawPals users “swipe right” on each other - AKA both users want
+        their dogs to play with each other - the users can message each other
+        and the site recommends dog parks in the area.
       </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <p>
+        The site also features an “adoption” mode, in which users interested in
+        adopting dogs can see rescues or other puppies up for adoption in the
+        area. They can then “swipe” right on dogs they are interested in
+        adopting, and, hopefully, find their perfect match.
+      </p>
+      <h3>Team Roles</h3>
+      <li>Marigrace Seaton: Client Manager, Frontend Engineer</li>
+      <li>Martin Smolka: Tech Lead</li>
+      <li>Alan Patterson: Fullstack Engineer</li>
+      <li>Casey Turgeon: Backend Developer</li>
+      <h3>Contact Info</h3>
+      Team email:{' '}
+      <a href='mailto: pawpalsdevelopment@gmail.com'>
+        pawpalsdevelopment@gmail.com
+      </a>
+      <p>
+        Client: Nash Philbeck:{' '}
+        <a href='mailto: hnashp@email.unc.edu'>hnashp@email.unc.edu</a>
+      </p>
+      <p>
+        {' '}
+        Marigrace Seaton:{' '}
+        <a href='mailto:mgseaton@email.unc.edu'>mgseaton@email.unc.edu</a>
+      </p>
+      <p>
+        Martin Smolka:{' '}
+        <a href='mailto:martin38@email.unc.edu'>martin38@email.unc.edu</a>
+      </p>
+      <p>
+        Alan Patterson:{' '}
+        <a href='mailto:walanpat@email.unc.edu'>walanpat@email.unc.edu</a>
+      </p>
+      <p>
+        Casey Turgeon:{' '}
+        <a href='mailto:cturgeon@email.unc.edu'>cturgeon@email.unc.edu</a>
+      </p>
+      <h3>Meetings Schedule</h3>
+      <p>First Client Meeting: Wednesday, February 3, 6 PM</p>
+      <p>Regular develo pment team meetings: Tuesdays, weekly, 5 PM</p>
+      <h3> Team Rules</h3>
+      <li>Behavior:</li>
+      <p>
+        Team members will communicate through the GroupMe we have set up. If a
+        team member will be late to a meeting, she or he should send a message
+        in the GroupMe in advance of the meeting.
+      </p>
+      <p>
+        Team members should respond to messages that pertain to them in the
+        GroupMe by the end of the workday that the message was sent, and no
+        response will normally be interpreted as the member’s not knowing the
+        answer to the question.
+      </p>
+      <p>
+        If team members are dissatisfied with the work of other members, she or
+        he can privately message them and express their concerns.
+      </p>
+      <p>
+        If a team member is unable to do her or his specified role appropriately
+        or encounters an obstacle in their role, the team member should ask for
+        assistance as soon as the obstacle is realized from other group members
+        in the GroupMe.
+      </p>
+      <li>Coding Practices</li>
+      <p>
+        As the team will not yet specify which language(s) we will use for the
+        project until we meet with our client, the team will hereby abide by the
+        following language conventions:
+      </p>
+      <p>
+        <a href='https://www.oracle.com/java/technologies/javase/codeconventions-contents.html'>
+          For Java
+        </a>
+      </p>
+      <p>
+        <a href='https://www.python.org/dev/peps/pep-0008/ '>For Python</a>
+      </p>
+      <p>
+        <a href='https://www.w3schools.com/js/js_best_practices.asp'>
+          For Javascript
+        </a>
+      </p>
     </main>
-  )
-}
+  );
+};
 
-export default IndexPage
+export default IndexPage;
